777b8ce65d74f6df353567296057723eb39c5d1c
Бируля Иван Александрович <birulya.i@tps.local>
2022-02-14 16:24:36 +0300

    Fixes

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -1,5 +1,3 @@
-from statistics import mean
-
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -11,13 +9,19 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
     def __str__(self):
-        grade_val = sum(self.grades.values())/len(self.grades.values())
-        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {grade_val}'
+        grade_sum = 0
+        grade_num = 0
+        for grade_list in list(self.grades.values()):
+            grade_sum += sum(grade_list)
+            grade_num += len(grade_list)
+        grade_stat = round(grade_sum/grade_num, 1)
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {grade_stat}'
         return res
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress and 0 <= grade <= 10:
+        if (isinstance(student, Student) and course in self.courses_attached
+            and course in student.courses_in_progress and 0 <= grade <= 10):
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -38,48 +42,58 @@ class Student:
         self.grades = {}
 
     def lecture_grade(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress and 0 <= grade <= 10:
+        if (isinstance(lecturer, Lecturer) and course in lecturer.courses_attached
+            and course in self.courses_in_progress and 0 <= grade <= 10):
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+    
+    def __str__(self):
+        grade_sum = 0
+        grade_num = 0
+        for grade_list in list(self.grades.values()):
+            grade_sum += sum(grade_list)
+            grade_num += len(grade_list)
+        if grade_num == 0:
+            grade_stat = "Нет оценок"
+        else:
+            grade_stat = round(grade_sum/grade_num, 1)
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\n\
+            \rСредняя оценка за домашние задания: {grade_stat}\n\
+            \rКурсы в процессе изучения: {" ".join(self.courses_in_progress)}\n\
+            \rЗавершенные курсы: {" ".join(self.finished_courses)}'
+        return res
         
 first_student = Student("Piter", "Ivanov", "man")
 second_student = Student("Vova", "Petrov", "man")
 first_student.courses_in_progress += ["Python"]
 second_student.courses_in_progress += ["Java"]
+first_student.finished_courses += ["Git"]
 
 green_lector = Lecturer("Sidor", "Petrovich")
 red_lector = Lecturer("Rinat", "Torvald")
 green_lector.courses_attached += ["Python"]
 red_lector.courses_attached += ["Java"]
+green_lector.courses_attached += ["Git"]
 
 old_reviwer = Reviewer("Varvara", "Reut")
 new_reviwer = Reviewer("Tolik", "Chekushin")
 old_reviwer.courses_attached += ["Python"]
 new_reviwer.courses_attached += ["Java"]
+old_reviwer.courses_attached += ["Git"]
 
 first_student.lecture_grade(green_lector, "Python", 10)
 print(green_lector.grades)
 
-new_reviwer.rate_hw(second_student, "Java", 10)
+new_reviwer.rate_hw(second_student, "Java", 8)
+old_reviwer.rate_hw(first_student, "Git", 7)
 print(second_student.grades)
+print(first_student.grades)
 
 print(old_reviwer)
 print(green_lector)
-
-    
- 
-# best_student = Student('Ruoy', 'Eman', 'your_gender')
-# best_student.courses_in_progress += ['Python']
- 
-# cool_mentor = Mentor('Some', 'Buddy')
-# cool_mentor.courses_attached += ['Python']
- 
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-# print(best_student.grades)
+print(second_student)
+print(first_student)
